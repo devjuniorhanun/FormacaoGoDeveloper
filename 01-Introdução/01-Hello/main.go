3ca4bdf1cc9891a4b4f0fd1321a6093ad560323c
Winston Hanun Júnior <devjuniorhanun@gmail.com>
2023-12-16 08:26:06 -0300

    Iniciando em Go...

---
+++ 01-Introdução/01-Hello/main.go
@@ -0,0 +1,13 @@
+// Pacote Principal
+package main
+
+// Importasndo pacotes Externos
+import "fmt"
+
+// Função Principal
+func main() {
+
+	// Imprine Hello World!
+	fmt.Println("Hello World!")
+
+}
